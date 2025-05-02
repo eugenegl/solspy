9b952c760bab8426886bb5832e75f0f91833deef
Yevhen Holota <golota.eg@gmail.com>
2025-05-02 17:42:17 +0300

    Wallet screen, static view, ver0.2

--- ios/SolSpy/SolSpy/View/TransactionDetails.swift
+++ ios/SolSpy/SolSpy/View/TransactionDetails.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TransactionDetails: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Transaction Details")
     }
 }
 
